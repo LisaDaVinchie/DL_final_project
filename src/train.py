986c698a2cb7c989b44069519a63eb0edda6b1c3
LisaDaVinchie <lisadavinchie@gmail.com>
2025-06-06 17:30:01 +0200

    Train code with small test

---
+++ src/train.py
@@ -0,0 +1,211 @@
+import torch as th
+import torch.optim as optim
+from pathlib import Path
+import argparse
+from time import time
+import json
+from import_dataset import create_dataloaders, load_cifar10_data
+from model import UNet
+from losses import select_loss_function
+
+def main():
+    """Main function to train a model on a dataset."""
+    start_time = time()
+    
+    parser = argparse.ArgumentParser(description='Train a CNN model on a dataset')
+    parser.add_argument('--paths', type=Path, help='Path to the JSON file with data paths')
+    parser.add_argument('--params', type=Path, help='Path to the JSON file with model parameters')
+    
+    args = parser.parse_args()
+    
+    params_file_path = Path(args.params)
+    paths_file_path = Path(args.paths)
+    if not params_file_path.exists():
+        raise FileNotFoundError(f"Parameters file not found: {params_file_path}")
+    if not paths_file_path.exists():
+        raise FileNotFoundError(f"Paths file not found: {paths_file_path}")
+    with open(params_file_path, 'r') as f:
+        params = json.load(f)
+    with open(paths_file_path, 'r') as f:
+        paths = json.load(f)
+    
+    batch_size = int(params["batch_size"])
+    loss_kind = str(params["loss_kind"])
+    epochs = int(params["epochs"])
+    learning_rate = float(params["learning_rate"])
+    
+    weights_path = Path(paths["next_weights_path"])
+    results_path = Path(paths["next_result_path"])
+    masks_path = Path(paths["current_mask_path"])
+    
+    if not weights_path.parent.exists():
+        raise FileNotFoundError(f"Weights directory does not exist: {weights_path.parent}")
+    if not results_path.parent.exists():
+        raise FileNotFoundError(f"Results directory does not exist: {results_path.parent}")
+    if not masks_path.exists():
+        raise FileNotFoundError(f"Masks file does not exist: {masks_path}")
+    
+    # Ensure the results file exists and is a txt file
+    results_path = results_path.with_suffix('.txt')
+    results_path.touch(exist_ok=True)
+        
+    
+    model = UNet()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+    loss = select_loss_function(loss_kind)
+        
+    train = TrainModel(model=model,
+                      loss_function=loss,
+                      optimizer=optimizer)
+    
+    trainset, testset = load_cifar10_data(normalize=True, n_train = 200, n_test = 10)
+    train_loader, test_loader = create_dataloaders(trainset, testset, batch_size=batch_size)
+    
+    train.train(train_loader, test_loader, epochs)
+    
+    elapsed_time = time() - start_time
+    print(flush=True)
+    train.save_weights()
+    print(f"Model weights saved at {weights_path}", flush=True)
+    
+    print(flush=True)
+    train.save_results(elapsed_time)
+    print(f"Results saved at {results_path}", flush=True)
+    
+    print(f"\nTraining completed in {elapsed_time:.2f} seconds", flush=True)
+
+def change_dataset_idx(dataset_path: Path, dataset_specs_path: Path, new_idx: int) -> tuple:
+    """Change the dataset index in the file names.
+
+    Args:
+        dataset_path (Path): latest dataset path
+        dataset_specs_path (Path): latest dataset specs path
+        new_idx (int): new dataset index
+
+    Raises:
+        FileNotFoundError: dataset file not found
+        FileNotFoundError: dataset specs file not found
+
+    Returns:
+        tuple: new dataset path, new dataset specs path
+    """
+    dataset_ext = dataset_path.suffix
+    dataset_name = dataset_path.stem.split("_")[0]
+    new_dataset_path = dataset_path.parent / f"{dataset_name}_{new_idx}{dataset_ext}"
+    
+    dataset_specs_ext = dataset_specs_path.suffix
+    dataset_specs_name = "dataset_specs"
+    
+    new_dataset_specs_path = dataset_specs_path.parent / f"{dataset_specs_name}_{new_idx}{dataset_specs_ext}"
+    
+    return new_dataset_path, new_dataset_specs_path
+    
+class TrainModel:
+    def __init__(self, model: th.nn.Module, loss_function, optimizer: th.optim, save_every: int = 1):
+        """Initialize the training class.
+
+        Args:
+            params (_type_): json
+            weights_path (_type_): _description_
+            results_path (_type_): _description_
+            dataset_specs (_type_, optional): _description_. Defaults to None.
+        """
+        
+        self.model = model
+        self.loss_function = loss_function
+        self.device = th.device("cuda" if th.cuda.is_available() else "cpu")
+        self.model.to(self.device)
+        self.optimizer = optimizer
+        self.lr = self.optimizer.param_groups[0]['lr']
+        self.save_every = save_every
+        
+        self.train_losses = []
+        self.test_losses = []
+        self.training_lr = []
+        
+    def train(self, train_loader, test_loader, epochs: int):
+        """Train the model on the dataset.
+
+        Args:
+            train_loader (th.utils.data.DataLoader): training dataloader
+            test_loader (th.utils.data.DataLoader): testing dataloader
+        """
+        
+        if epochs <= 0:
+            raise ValueError("Number of epochs must be positive.")
+        
+        print(flush=True)
+        for epoch in range(epochs):
+            print(f"Epoch {epoch + 1}/{epochs}\n", flush=True)
+            self.model.train()
+            epoch_loss = 0.0
+            n_batches = 0
+            for (images, masks) in train_loader:
+                loss = self._compute_loss(images, masks)
+                epoch_loss += loss.item()
+                
+                loss.backward()
+                self.optimizer.step()
+                self.optimizer.zero_grad()
+                n_batches += 1
+
+            self.training_lr.append(self.optimizer.param_groups[0]['lr'])
+            # self.scheduler.step() if self.scheduler is not None else None
+            
+            self.train_losses.append(epoch_loss / n_batches)
+            
+            with th.no_grad():
+                self.model.eval()
+                epoch_loss = 0.0
+                n_batches = 0
+                for (images, masks) in test_loader:
+                    loss = self._compute_loss(images, masks)
+                    epoch_loss += loss.item()
+                    n_batches += 1
+                self.test_losses.append(epoch_loss / n_batches)
+            
+            if (epoch + 1) % self.save_every == 0:
+                self.save_weights()
+                self.save_results()
+                print(f"\nModel weights and results saved at epoch {epoch + 1}\n", flush=True)
+                
+        print(flush=True)
+                
+    def _compute_loss(self, images, masks):
+        images = images.to(self.device)
+        masks = masks.to(self.device)
+        output = self.model(images, masks.float())
+        loss = self.loss_function(output[:, 0], images[:, 4], masks[:, 4])
+        return loss
+    
+    def save_weights(self, weights_path):
+        """Save the model weights to a file."""
+        th.save(self.model.state_dict(), weights_path)
+        
+    def save_results(self, results_path: Path,  elapsed_time: float = None):
+        """Save the training results to a file.
+
+        Args:
+            elapsed_time (float): elapsed time of the training
+        """
+        
+        # Save the train losses to a txt file
+        with open(results_path, 'w') as f:
+            if elapsed_time is not None:
+                f.write("Elapsed time [s]:\n")
+                f.write(f"{elapsed_time}\n\n")
+            f.write("Train losses\n")
+            for loss in self.train_losses:
+                f.write(f"{loss}\t")
+            f.write("\n\n")
+            f.write("Test losses\n")
+            for loss in self.test_losses:
+                f.write(f"{loss}\t")
+            f.write("\n\n")
+            f.write("Learning rate\n")
+            for lr in self.training_lr:
+                f.write(f"{lr}\t")
+            f.write("\n\n")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/train_test.py
@@ -0,0 +1,24 @@
+import torch as th
+from torch.optim import Adam
+import unittest
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+from train import TrainModel
+from model import SimpleModel
+from losses import PerPixelL1
+
+class TestTrainModel(unittest.TestCase):
+    def setUp(self):
+        """Set up the test environment."""
+        self.model = SimpleModel()
+        self.loss_function = PerPixelL1()
+        self.optimizer = Adam(self.model.parameters(), lr=0.001)
+        self.train_model = TrainModel(self.model, self.loss_function, self.optimizer)
+
+    def test_train_model_initialization(self):
+        """Test if the TrainModel class initializes correctly."""
+        self.assertIsNotNone(self.train_model)
+        self.assertEqual(len(self.train_model.train_losses), 0)
+        self.assertEqual(len(self.train_model.test_losses), 0)
